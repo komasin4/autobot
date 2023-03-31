c00c0bce153e315f90cb095a19eda063de87b499
jhpark <komasin4@gmail.com>
2023-03-31 14:38:26 +0900

    텔레그램 전송 추가

--- rsitest.py
+++ rsitest.py
@@ -16,6 +16,7 @@ rsi_pre_old2 = 0
 trade = 'N'
 price_unit = 5000
 
+#key값을 읽어옴
 f = open("key.txt", 'r')
 while True:
     line = f.readline()
@@ -32,11 +33,9 @@ while True:
         chatid = strParse[1]
 f.close()
 
-#print(access)
-#print(secret)
-
 upbit = pyupbit.Upbit(access, secret)  # 업비트 객체를 만듭니다.
 
+#텔레그램 메시지 전송
 def telegram_send(token, chatid, msg):
     API_HOST = "https://api.telegram.org/bot"
     url = API_HOST + token + '/sendmessage'
@@ -65,7 +64,6 @@ def GetRSI(ohlcv, period):
     RS = _gain / _loss
     return pd.Series(100 - (100 / (1 + RS)), name="RSI")
 
-
 def GetTotalRealMoney(balances):
     total = 0.0
     for value in balances:
@@ -91,7 +89,6 @@ def GetTotalRealMoney(balances):
 
     return total
 
-
 def getAmount(type):
     type_ratio = {"BUY": 0.05, "SELL": 0.05}.get(type, 0)
     total = GetTotalRealMoney(upbit.get_balances())
@@ -102,9 +99,11 @@ def Monitor():
     global rsi_pre_old2
     global trade
     global price_unit
+    global token
+    global chatid
     
-    strSend = "%s,%f,%f"%("메시지", 1.2, 1.3)
-    telegram_send(token, chatid, strSend)
+    #strSend = "%s,%f,%f"%("메시지", 1.2, 1.3)
+    #telegram_send(token, chatid, strSend)
     
     df = pyupbit.get_ohlcv("KRW-BTC", interval="minute1")
 
@@ -123,23 +122,31 @@ def Monitor():
     if(rsi_pre_old != rsi_pre and rsi_pre_old > 0): #최초 실행시 (rsi_pre_old == 0) 일때 매매하지 않도록
         rsiChangeString = "change:Y traded:" + trade
         trade = 'N'
+        sendMsg = ""
+        cnt = 0
+        order = 0
         if (rsi_pre > 70 and rsi_pre_old <= 70 and trade == 'N'):
-            sell_cnt = round(getAmount("SELL") / now_price, 8)
-            print(upbit.sell_limit_order(
-                "KRW-BTC", now_price-price_unit, sell_cnt))
+            cnt = round(getAmount("SELL") / now_price, 8)
+            order = now_price-price_unit
+            print(upbit.sell_limit_order("KRW-BTC", order, cnt))
             addString = "sell - golden cross!!!"
             trade = 'Y'
         elif (rsi_pre <= 70 and rsi_pre_old > 70 and trade == 'N'):
-            sell_cnt = round(getAmount("SELL") / now_price, 8)
-            print(upbit.sell_limit_order(
-                "KRW-BTC", now_price-price_unit, sell_cnt))
+            cnt = round(getAmount("SELL") / now_price, 8)
+            order = now_price-price_unit
+            print(upbit.sell_limit_order("KRW-BTC",odrder, cnt))
             addString = "sell - dead cross!!!"
             trade = 'Y'
         elif (rsi_pre >= 30 and rsi_pre_old < 30 and trade == 'N'):
-            buy_cnt = round(getAmount("BUY") / now_price, 8)
-            print(upbit.buy_limit_order("KRW-BTC", now_price+price_unit, buy_cnt))
+            cnt = round(getAmount("BUY") / now_price, 8)
+            order = now_price+price_unit
+            print(upbit.buy_limit_order("KRW-BTC", order, cnt))
             addString = "buy - golden cross!!!"
             trade = 'Y'
+        if(sendMsg != ""):
+            print(sendMsg)
+            sendMsg = addString + str(order) + ":" + str(cnt) + ":" + str(order*cnt)
+            telegram_send(token, chatid, sendMsg)
         rsi_pre_old = rsi_pre
         rsiChange = True
     elif(rsi_pre_old == 0): #최초 실행시는 rsi_pre_old = rsi_pre 어사인.
@@ -154,7 +161,6 @@ def Monitor():
         print(datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S '),
             rsi_pre_old2, "->", rsi_pre, "->", rsi_now, now_price, addString, flush=True)
             #rsi_pre_old2, ":", rsi_pre_old, "->", rsi_pre, "->", rsi_now, now_price, rsiChangeString, addString, flush=True)
-
 
 print("Start Bot at ", datetime.now(
     timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S\t'), flush=True)
